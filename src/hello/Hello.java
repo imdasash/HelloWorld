204899fa83c658347b674b3279af78832b016881
Oleksandr Danylchuk <imdasash@gmail.com>
2018-06-10 12:05:57 -0400

    added HelloWorld project

---
+++ src/hello/Hello.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+		int a = 10;
+		 a++;
+		System.out.println("Hello world!");
+	     
+	}
+
+}
